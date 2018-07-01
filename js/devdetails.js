c1052d193b8655988cd506c9117b7f840ed479eb
hptaist <hptaist@gmail.com>
2018-07-01 18:33:56 +0700

    index1.html + devdetail.html

--- js/devdetails.js
+++ js/devdetails.js
@@ -1,76 +1,89 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
-var object,serverLink='http://10.71.1.75:8080/user/distributor';
+(function ($) {
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    var object,serverLink='http://10.71.1.75:8080/user/distributor';
 
-var getDeviceDetails = function(devmac){
-    $.ajax({
-        url: serverLink+':'+devmac,
-        data: {
-            mac: devmac,
-        },
-        method: "GET",
-        success: function(data){
-            object=data;
-            $("#dmac-td").html(data.device.mac);
-            $("#dname-td").html(data.device.machine_type);
-            $("#dfirm-td").html(data.device.firmware);
-            $("#dmem-td").html(data.device.free_ram+"/"+data.device.total_ram);
-    
-            $.each(data.dhcp,function(){
-                $("#dhcp-panel").append('<button class="collapsible">'+ this.ip+'</button><div class="content"><p>MAC: '+this.mac+'</p><p>Name: '+this.name+'</p></div>');
-            })
-            $.each(data.splash,function(){
-                if(this.auth_state=="pass"){
-                    $("#splash-panel").append('<button class="collapsible">'+ this.ip+'</button><div class="content"><p>MAC: '+this.mac+'</p></div>');
-                }            
-            })
-            $.each(data.ssids, function(){
-                var tr='<tr><td>'+this.interface+'</td><td>'+this.ssid+'</td><td><button id="edit-btn" type="button" class="btn btn-info" data-toggle="modal" data-target="#ssid-modal">Edit</button></td></tr>';
-                $("#ssids-table tbody").append(tr);
-            }) 
-        }
-    })
-}
-$(document).on('click', ".collapsible", function(){
-    this.classList.toggle("active");
+    var getDeviceDetails = function(devmac){
+        $.ajax({
+            url: serverLink+':'+devmac,
+            data: {
+                mac: devmac,
+            },
+            method: "GET",
+            success: function(data){
+                object=data;
+                $("#device-name").html(data.device.machine_type);
+                $("#dmac-td").html(data.device.mac);
+                $("#dname-td").html(data.device.machine_type);
+                $("#dfirm-td").html(data.device.firmware);
+                $("#dmem-td").html(data.device.free_ram+"/"+data.device.total_ram);
+                var dhcpdata="";
+                $.each(data.dhcp,function(){
+                    var tr = '<tr><td>' + this.ip + '</td><td>' + this.mac + '</td><td>' + this.name + '</td></tr>';
+                    dhcpdata=dhcpdata+tr;
+                    $("#dhcp-table tbody").html(dhcpdata);
+                })
+                $.each(data.splash,function(){
+                    if(this.auth_state=="pass"){
+                        $("#splash-panel").append('<button class="collapse">'+ this.ip+'</button><div class="content"><p>MAC: '+this.mac+'</p></div>');
+                    }
+                })
+                $.each(data.ssids, function(){
+                    if(this.signal==null){
+                        var tr = '<tr><td>' + this.interface + '</td><td>' + this.ssid + '</td><td>' + this.channel + '</td><td>' + this.signal + '</td><td>' + this.tx_power + '</td><td><span class="badge badge-danger">OFF</span></td><td><button id="edit-btn" type="button" class="btn btn-info" data-toggle="modal" data-target="#ssid-modal">Edit</button></td></tr>';
+                        $("#ssids-table tbody").append(tr);
+                    }
+                    else {
+                        var tr = '<tr><td>' + this.interface + '</td><td>' + this.ssid + '</td><td>' + this.channel + '</td><td>' + this.signal + '</td><td>' + this.tx_power + '</td><td><span class="badge badge-success">ON</span></td><td><button id="edit-btn" type="button" class="btn btn-info" data-toggle="modal" data-target="#ssid-modal">Edit</button></td></tr>';
+                        $("#ssids-table tbody").append(tr);
+                    }
+                })
+            }
+        })
+    }
+    $(document).on('click', ".collapsible", function(){
+        this.classList.toggle("active");
         var content = this.nextElementSibling;
         if (content.style.maxHeight){
             content.style.maxHeight = null;
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
         }
-});
+    });
 
-$("#ssids-table tbody").on('click','button',function(){
-  //  $("#modal-bod").empty();
-    var col,txt;
-    dinterface = $(this).parent().siblings("td").html();
-    console.log(dinterface);
-    $.each(object.ssids,function(){
-        if (this.interface==dinterface){
-            $("#modal-interface").val(this.interface);
-            $("#modal-channel").val(this.channel);
-            $("#modal-ssid").val(this.ssid);
-            if(this.bssid){
-                $("#modal-bssid").val(this.bssid);
+    $("#ssids-table tbody").on('click','button',function(){
+        //  $("#modal-bod").empty();
+        var col,txt;
+        dinterface = $(this).parent().siblings("td").html();
+        console.log(dinterface);
+        $.each(object.ssids,function(){
+            if (this.interface==dinterface){
+                $("#modal-title").html('Edit Interface '+this.interface);
+                $("#interface").val(this.interface);
+                $("#channel").val(this.channel);
+                $("#ssid").val(this.ssid);
+
             }
-        }
-        
+
+        })
+
     })
 
-})
+    $("#modal-bod input").on('change', function(){
+        $(this).addClass('changed');
+    })
 
-$("#modal-bod input").on('change', function(){
-    $(this).addClass('changed');
-})
+    $("#myform").on('submit', function(){
+        alert("abc");
+        $('input:not(.changed)').prop('disabled',true);
+        $('#interface').prop('disabled',false);
+    })
 
-$("#myform").on('submit', function(){
-    $('input:not(.changed)').prop('disabled',true);
-    $('#modal-interface').prop('disabled',false);
-})
+    $(document).ready(function(){
+        var currentLocation=document.location.toString();
+        var mac=currentLocation.split("?")[1];
+        getDeviceDetails(mac);
+        //$("#wireless").attr("href","devdetail.html?"+mac);
 
-$(document).ready(function(){
-    var currentLocation=document.location.toString();
-    var mac=currentLocation.split("?")[1];
-    getDeviceDetails(mac);
-})
+    })
+}(jQuery))
--- js/devinfo.js
+++ js/devinfo.js
@@ -1,67 +1,73 @@
-const serverLink="http://10.71.1.75:8080/user/distributor";
-var currentURL = window.location.hostname;
-var object,i=1;
+(function ($) {
+    var currentURL = window.location.hostname;
+    var object;
 
-var getDeviceList = function(){
-    i=1;
-    $("#device-list-table tbody").empty();
-    $.ajax({
-        url: serverLink,
-        method: "GET",
-        success: function(data){        
-            $.each(data,function() {
-                var ts=new Date(1000*this.device.created_at);
-                var ts2=new Date(this.device.created_at);
-                var diff=Date.now();
-                diff=~~((diff-ts) /1000);
-                var datestring=ts.toLocaleString();
-                var machine_type=this.device.machine_type;
-                machine_type = machine_type.replace(/^\n|\n$/g, '');
-                var row;
-                if (diff<45){
-                    row='<tr><td>'+i+'</td><td>'+machine_type+'</td><td>'+this.device.mac+'</td><td>'+this.device.wan_ip+'</td><td>'+datestring+'</td><td><span class="label label-success">OK</span></td></tr>'
-                }
-                else{
-                    row='<tr><td>'+i+'</td><td>'+machine_type+'</td><td>'+this.device.mac+'</td><td>'+this.device.wan_ip+'</td><td>'+datestring+'</td><td><span class="label label-danger">Warning</span></td></tr>'
-                }
-                $("#device-list-table tbody").append(row);
-                $("#mac-select").append('<option>'+this.device.mac+'</option>');
-                i++;
-            });
-        },
+    var getDeviceList = function(){
+        var i=1;
+        var tabledata="";
+        /*$("#bootstrap-data-table tbody").html("");*/
+        $.ajax({
+            url: "http://10.71.1.75:8080/user/distributor",
+            method: "GET",
+            success: function(data){
+                $.each(data,function() {
+                    var ts=new Date(1000*this.device.created_at);
+                    var ts2=new Date(this.device.created_at);
+                    var diff=Date.now();
+                    diff=~~((diff-ts) /1000);
+                    var datestring=ts.toLocaleString();
+                    var machine_type=this.device.machine_type;
+                    machine_type = machine_type.replace(/^\n|\n$/g, '');
+                    var row;
+                    if (diff%2){
+                        row='<tr><td>'+i+'</td><td>'+machine_type+'</td><td>'+this.device.mac+'</td><td>'+this.device.wan_ip+'</td><td>'+datestring+'</td><td><span class="label label-success">Connected</span></td></tr>'
+                    }
+                    else{
+                        row='<tr><td>'+i+'</td><td>'+machine_type+'</td><td>'+this.device.mac+'</td><td>'+this.device.wan_ip+'</td><td>'+datestring+'</td><td><span class="label label-danger">Disconnected</span></td></tr>'
+                    }
+                    /*$("#bootstrap-data-table tbody").append(row);*/
+
+                    tabledata = tabledata + row;
+                    $("#bootstrap-data-table tbody").html(tabledata);
+                    $("#mac-select").append('<option>'+this.device.mac+'</option>');
+                    i++;
+                });
+            },
+        });
+
+
+
+
+    }
+
+    window.setInterval(function(){
+        getDeviceList();
+    }, 1000);
+
+    $(document).ready(function(){
+        getDeviceList();
+    })
+
+    $("#send-multiple").on('click',function(){
+        $("#command-area").val('');
+    })
+
+    $("#bootstrap-data-table tbody").on("click", "tr",function(){
+        var mac_td=this.cells[2];
+        var mac = $(mac_td).text();
+        /*  getDeviceDetails(mac); */
+
+        window.location = "devdetail.html?"+mac;
+        //window.location = "google.com"
     });
 
-
-
-}
-
-window.setInterval(function(){
-    getDeviceList();
-  }, 5000);
-
-$(document).ready(function(){
-    getDeviceList();
-})
-
-$("#send-multiple").on('click',function(){
-    $("#command-area").val('');
-})
-
-$("#device-list-table").on("click", "tr",function(){
-    var mac_td=this.cells[2];
-    var mac = $(mac_td).text();
-   /*  getDeviceDetails(mac); */
-   
-   window.location = "/details.html?"+mac;
-});
-
-$("#myform").submit(function(e){
-    var $inputs= $("#myform :input");
-    $inputs.each(function(){
-        if (this.value==""){
-            e.preventDefault();
-            return false;
-        }
+    $("#myform").submit(function(e){
+        var $inputs= $("#myform :input");
+        $inputs.each(function(){
+            if (this.value==""){
+                e.preventDefault();
+                return false;
+            }
+        })
     })
-})
-
+}(jQuery))
